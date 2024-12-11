3647f6ce6b1660aa8ff15923b3dc6641edeb4293
wsafight <984292420@qq.com>
2024-12-11 23:49:44 +0800

    chore:init project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,6 @@ import java.io.File
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!")
-
     if (args.size < 2) {
         System.err.println("Usage: ./your_program.sh tokenize <filename>")
         exitProcess(1)
@@ -21,9 +18,9 @@ fun main(args: Array<String>) {
     val fileContents = File(filename).readText()
 
     // Uncomment this block to pass the first stage
-    // if (fileContents.isNotEmpty()) {
-    //     throw NotImplementedError("Scanner not implemented")
-    // } else {
-    //     println("EOF  null") // Placeholder, remove this line when implementing the scanner
-    // }
+    if (fileContents.isNotEmpty()) {
+        throw NotImplementedError("Scanner not implemented")
+    } else {
+        println("EOF  null") // Placeholder, remove this line when implementing the scanner
+    }
 }
